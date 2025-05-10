878f0afa1f3eabb8a1da5eb34e608008506ee3d8
Craig Troop <kr3kls@gmail.com>
2025-05-09 21:14:47 -0400

    architecture diagram

---
+++ SEAS-8405/HW7/diagram.py
@@ -0,0 +1,29 @@
+from diagrams import Cluster, Diagram
+from diagrams.programming.framework import Flask
+from diagrams.onprem.network import Nginx
+from diagrams.onprem.database import PostgreSQL
+from diagrams.onprem.container import Docker
+from diagrams.onprem.client import User
+
+with Diagram(
+    "Flask App Architecture in Docker",
+    filename="architecture_diagram",
+    outformat="png",
+    show=False
+):
+    user = User("Local Browser")
+
+    with Cluster("Host System"):
+        docker = Docker("Docker Daemon")
+
+        with Cluster("Docker Container: web"):
+            nginx = Nginx("Nginx (port 15000)")
+            flask = Flask("Flask App (port 5000)")
+            nginx >> flask
+
+        with Cluster("Docker Container: db"):
+            db = PostgreSQL("PostgreSQL (port 5432)")
+
+        docker >> nginx
+        flask >> db
+        user >> nginx
