53e6efa4b522123e160140eed33020d29657d580
kr3kls <kr3kls@gmail.com>
2025-05-23 12:35:48 -0400

    working java app

---
+++ SEAS-8405/HW9/before/src/main/java/com/seas8405/Log4ShellDemo.java
@@ -0,0 +1,11 @@
+package com.seas8405;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4ShellDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(Log4ShellDemo.class, args);
+    }
+}
