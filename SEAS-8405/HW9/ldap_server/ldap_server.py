2994795fc91e9f20d03f7d3c1e7f36f4a32fa0e5
kr3kls <kr3kls@gmail.com>
2025-05-24 08:45:56 -0400

    ldap server enhancement

--- SEAS-8405/HW9/ldap_server/ldap_server.py
+++ SEAS-8405/HW9/ldap_server/ldap_server.py
@@ -3,19 +3,20 @@ import socket
 def main():
     HOST = '172.17.0.1'
     PORT = 1389
-
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
-        s.listen(1)
-        print(f"[+] LDAP server listening on {HOST}:{PORT}")
+        while(True):
+            s.listen(1)
+            print(f"[+] LDAP server listening on {HOST}:{PORT}")
 
-        conn, addr = s.accept()
-        with conn:
-            print(f"[+] Accepted connection from {addr}")
-            data = conn.recv(1024)
-            print(f"[+] Received data: {data.hex()}")
+            conn, addr = s.accept()
+            with conn:
+                print(f"[+] Accepted connection from {addr}")
+                data = conn.recv(1024)
+                print(f"[+] Received data: {data.hex()}")
 
-            # conn.close()
+                conn.close()
 
 if __name__ == '__main__':
     main()
